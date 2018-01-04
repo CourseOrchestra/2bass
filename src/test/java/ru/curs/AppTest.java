c4110f41c18d3cd1d5dab5f3337b9fc57082d829
inponomarev <iponomarev@mail.ru>
2018-01-04 20:20:32 +0300

    initial commit for project 2bass

---
+++ src/test/java/ru/curs/AppTest.java
@@ -0,0 +1,18 @@
+package ru.curs;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
