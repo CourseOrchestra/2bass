b406355960fd0ca22dc54baab57987290884c668
ioann <ioanngolovko@gmail.com>
2018-01-23 20:21:28 +0300

    1. appassembler maven plugin was added 2. Configuration was implemented 3. Simple "help" was added

--- src/main/java/ru/curs/bass/App.java
+++ src/main/java/ru/curs/bass/App.java
@@ -1,57 +1,84 @@
 package ru.curs.bass;
 
+import info.macias.kaconf.Configurator;
+import info.macias.kaconf.ConfiguratorBuilder;
+import info.macias.kaconf.sources.JavaUtilPropertySource;
 import ru.curs.celesta.*;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.function.Consumer;
 
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
+public class App {
+
+    private static final String PROPERTIES_ENV_KEY = "BASS_PROPERTIES";
+
+    private static final String HELP = "Available commands are:\n\t\t"
+            + "import\n\t\t"
+            + "plan\n\t\t"
+            + "apply";
 
     private static final Map<String, Consumer<Bass>> TASKS = new HashMap<>();
 
     static {
+        TASKS.put(Task.IMPORT.toString(), Bass::toString); //TODO:
+        TASKS.put(Task.PLAN.toString(), Bass::toString);  //TODO:
         TASKS.put(Task.APPLY.toString(), Bass::updateDb);
     }
 
-    public static void main( String[] args ) throws CelestaException
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws CelestaException {
+        System.out.println("Hello World!");
 
-        String task = task(args);
+        if (args.length == 0) {
+            System.out.println("No command was specified.\n" + HELP);
+            return;
+        }
+        String task = args[0];
         Consumer<Bass> bassConsumer = TASKS.get(task);
 
         if (bassConsumer == null) {
-            throw new BassException("Invalid command was specified");
+            System.out.println("Invalid command was specified.\n" + HELP);
+        } else {
+            String propertiesPath = System.getenv(PROPERTIES_ENV_KEY);
+            AppProperties properties = readProperties(propertiesPath);
+            Bass bass = new Bass(properties);
+            bassConsumer.accept(bass);
+        }
+    }
+
+    private static AppProperties readProperties(String propertiesPath) {
+
+        File propertiesFile = new File(propertiesPath);
+
+        try (FileInputStream fis = new FileInputStream(propertiesFile.getAbsolutePath())) {
+            AppProperties properties = new AppProperties();
+
+            Properties propsFromFile = new Properties();
+            propsFromFile.load(fis);
+            Configurator configurator = new ConfiguratorBuilder()
+                    .addSource(new JavaUtilPropertySource(propsFromFile))
+                    .build();
+
+            configurator.configure(properties);
+            return properties;
+        } catch (FileNotFoundException e) {
+            throw new BassException("There is no properties file on path " + propertiesPath);
+        } catch (IOException e) {
+            throw new BassException(e);
         }
 
-        AppProperties properties = readProperties();
-        Bass bass = new Bass(properties);
-        bassConsumer.accept(bass);
     }
 
-    private static String task(String[] args) {
-        if (args.length == 0)
-            throw new BassException("Command wasn't specified");
-
-        return args[0];
-    }
-
-
-    private static AppProperties readProperties() { //TODO!!!
-        AppProperties properties = new AppProperties();
-        properties.setScorePath("score");
-        properties.setJdbcUrl("jdbc:h2:mem:celesta;DB_CLOSE_DELAY=-1");
-        return properties;
-    }
 
     private enum Task {
+        IMPORT,
+        PLAN,
         APPLY;
 
         @Override
